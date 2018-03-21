5df7a1a13bb251f48220087b9af28010e10a88c1
HuyTrinh <dinhhuyams@gmail.com>
2018-03-21 07:15:55 +0200

    add favicon

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-
+  /* sticky navigation*/
   $('.js--section-features').waypoint(function(direction) {
     if (direction == 'down') {
       $('nav').addClass('sticky');
@@ -7,8 +7,71 @@ $(document).ready(() => {
       $('nav').removeClass('sticky');
     }
   }), {
-    offset: '60px'
-  }
+    offset: '60px',
+  };
+
+  /*Scroll on buttons*/
+  $('.js--scroll-to-plans').click(() => {
+    console.log('SCROLL');
+    $('html, body').
+        animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-start').click(() => {
+    console.log('SCROLL');
+    $('html, body').
+        animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+  /*Navigation scrolls*/
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+      .not('[href="#"]').not('[href="#0"]').click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top,
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  /*Animation on scrolls*/
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
 
 });
 
